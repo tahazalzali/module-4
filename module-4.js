260c66fe9db28660d3240f9cc6ea86df44d05d0d
tahazalzali <tahazalzaly51@gmail.com>
2021-04-30 01:09:26 +0300

    f

---
+++ module-4.js
@@ -0,0 +1,12 @@
+
+var a =function(name8){
+    var name8=["allow","jallow","jabil"];
+    for (var i=0;i<name8.length;i++){
+    var fl = name8[i].charAt(0).toLowerCase();
+    if (fl=="j"){
+console.log("Hello "+name8[i] );}
+else {
+console.log("Goodbye "+name8[i]);}
+}
+}
+a();
